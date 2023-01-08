575f950c2e6e92f515b63b3f9b8540fcf8853de5
Sina Madadi <sinamadadi.work@gmail.com>
2023-01-08 21:10:49 +0330

    project created !

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <Header />
+      </h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+render(<App />, document.getElementById('root'));
+
