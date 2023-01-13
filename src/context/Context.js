13bb1793f03c5f116b365070db25fb7557d093c6
Sina Madadi <sinamadadi.work@gmail.com>
2023-01-13 22:01:31 +0330

    fixing

--- src/context/Context.js
+++ src/context/Context.js
@@ -3,13 +3,14 @@ import { faker } from '@faker-js/faker';
 import { cartReducer } from './Reducer';
 
 const Cart = createContext();
+faker.seed(99);
 
 const Context = ({ children }) => {
     const products = [...Array(20)].map(() => ({
         id: faker.datatype.uuid(),
         name: faker.commerce.productName(),
         price: faker.commerce.price(),
-        image: faker.image.abstract(),
+        image: faker.image.image(),
         inStock: faker.helpers.arrayElement([0, 3, 5, 6, 7]),
         fastDelivery: faker.datatype.boolean(),
         ratings: faker.helpers.arrayElement([1, 2, 3, 4, 5]),
--- src/context/Reducer.js
+++ src/context/Reducer.js
@@ -1,7 +1,15 @@
 export const cartReducer = (state, action) => {
     switch (action.type) {
-        // case :
-
+        case 'ADD_TO_CART':
+            return {
+                ...state,
+                Cart: [...state.cart, { ...action.payload, qty: 1 }]
+            };
+        case 'REMOVE_FROM_CART':
+            return {
+                ...state,
+                Cart: state.cart.filter((c) => c.id !== action.payload.id)
+            };
 
         default:
             return state;
